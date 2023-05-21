40f900fc80d91ab2f2572deabc4ad3d9e5dfdc5b
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-21 15:41:32 -0400

    installed browser router and created routes

--- pilates-haven-front-end/src/App.js
+++ pilates-haven-front-end/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Show from "./Pages/Show";
+import Index from "./Pages/Index";
+import Update from "./Pages/Update";
+import New from "./Pages/New";
+import NotFound from "./Pages/NotFound";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/studios" element={<Show />} />
+        <Route path="/studios/:id" element={<Index />} />
+        <Route path="/studios/:id/edit" element={<Update />} />
+        <Route path="/studios/new" element={<New />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ pilates-haven-front-end/src/Pages/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <>"Welcome to Pilates Have NYC!"</>;
+};
+
+export default Home;
