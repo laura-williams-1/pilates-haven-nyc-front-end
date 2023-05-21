a462da326d77537caec74ad73805f76bedf6a1e7
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-21 18:50:48 -0400

    added nav bar to site

--- pilates-haven-front-end/src/App.js
+++ pilates-haven-front-end/src/App.js
@@ -6,18 +6,25 @@ import Index from "./Pages/Index";
 import Update from "./Pages/Update";
 import New from "./Pages/New";
 import NotFound from "./Pages/NotFound";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/studios" element={<Index />} />
-        <Route path="/studios/:id" element={<Show />} />
-        <Route path="/studios/:id/edit" element={<Update />} />
-        <Route path="/studios/new" element={<New />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <header>
+        <NavBar />
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/studios" element={<Index />} />
+          <Route path="/studios/:id" element={<Show />} />
+          <Route path="/studios/:id/edit" element={<Update />} />
+          <Route path="/studios/new" element={<New />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </div>
   );
 }
