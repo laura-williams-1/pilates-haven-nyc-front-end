e71b14caf67ebb0d3a06c68cfa6b016b73160df5
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-22 08:21:39 -0400

    studio detail page finished

--- pilates-haven-front-end/src/Components/Studio.js
+++ pilates-haven-front-end/src/Components/Studio.js
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 const Studio = ({ studio }) => {
   return (
     <div className="studio">
-      <Link to={`/studios/${studio.id}`}> </Link>
-      <div className="studio-details">
-        <h1>{studio.name}</h1>
-        <p>{studio.address}</p>
-        <p>
-          {studio.city}, {studio.state} {studio.zipcode}
-        </p>
-        <p>Phone: {studio.phone_number}</p>
-      </div>
+      <Link to={`/studios/${studio.id}`}>
+        <div className="studio-details">
+          <h1>{studio.name}</h1>
+          <p>{studio.address}</p>
+          <p>
+            {studio.city}, {studio.state} {studio.zipcode}
+          </p>
+          <p>Phone: {studio.phone_number}</p>
+        </div>
+      </Link>
     </div>
   );
 };
--- pilates-haven-front-end/src/Components/StudioDetails.js
+++ pilates-haven-front-end/src/Components/StudioDetails.js
@@ -1,32 +1,127 @@
+import axios from "axios";
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./StudioDetails.css";
+const API = process.env.REACT_APP_API_URL;
 const StudioDetails = () => {
+  const [studio, setStudio] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+  console.log(API);
+  useEffect(() => {
+    axios
+      .get(`${API}/studios/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        setStudio(res.data);
+      })
+      .catch((e) => {
+        console.warn("error", e);
+      });
+  }, [id]);
+
+  const deleteStudio = () => {
+    axios
+      .delete(`${API}/studios/${id}`)
+      .then(() => navigate(`/studios`))
+      .catch((e) => console.warn("error", e));
+  };
+
   return (
     <>
-      {/* <div className="studio">
+      <div className="studio">
         <h1>{studio.name}</h1>
+        <h5> "{studio.description}"</h5>
         <p>{studio.address}</p>
         <p>
           {studio.city}, {studio.state} {studio.zipcode}
         </p>
         <p>Phone: {studio.phone_number}</p>
         <p>Email: {studio.email}</p>
-        <p>Website: {studio.website_url}</p>
-        <p>Instagram: {studio.instagram_handle}</p>
-        <p>Description: {studio.description}</p>
-        <p>Speciaulzation: {studio.speciaulzation}</p>
-        <p>Offers:</p>
-        <ul>
-          <ul>Group Classes: {studio.offers_group ? "Yes" : "No"}</ul>
-          <ul>Mat Classes: {studio.offers_mat ? "Yes" : "No"}</ul>
-          <ul>Reformer Classes: {studio.offers_reformer ? "Yes" : "No"}</ul>
-          <ul>Duet Classes: {studio.offers_duets ? "Yes" : "No"}</ul>
-          <ul>Private Classes: {studio.offers_privates ? "Yes" : "No"}</ul>
-          <ul>Membership: {studio.offers_membership ? "Yes" : "No"}</ul>
+        <p>
+          <a
+            href={studio.website_url}
+            onClick={(e) => {
+              if (
+                !window.confirm("Are you sure you want to leave this page?")
+              ) {
+                e.preventDefault();
+              }
+            }}
+          >
+            <a href={studio.website_url}>Website</a>
+          </a>
+        </p>
+        <p>
+          {studio.instagram_handle
+            ? `Instagram: ${studio.instagram_handle} `
+            : null}
+        </p>
+
+        <p>Specilization: {studio.specialization}</p>
+
+        <div className="Offers">
+          <h3>Offers</h3>
           <ul>
-            New Culent Deal: {studio.offers_new_culent_deal ? "Yes" : "No"}
+            {studio.offers_group && (
+              <li>
+                <span className="checkmark">✓</span>
+                Group Classes
+              </li>
+            )}
+            {studio.offers_mat && (
+              <li>
+                <span className="checkmark">✓</span>
+                Mat Classes
+              </li>
+            )}
+            {studio.offers_reformer && (
+              <li>
+                <span className="checkmark">✓</span>
+                Reformer Classes:
+              </li>
+            )}
+            {studio.offers_duets && (
+              <li>
+                <span className="checkmark">✓</span>
+                Duet Classes:
+              </li>
+            )}
+            {studio.offers_privates && (
+              <li>
+                <span className="checkmark">✓</span>
+                Private Classes
+              </li>
+            )}
+            {studio.offers_membership && (
+              <li>
+                <span className="checkmark">✓</span>
+                Membership
+              </li>
+            )}
+            {studio.offers_new_culent_deal && (
+              <li>
+                <span className="checkmark">✓</span>
+                New Client Deal
+              </li>
+            )}
+            {studio.is_black_owned && (
+              <li>
+                <span className="checkmark">✓</span>
+                Black-owned
+              </li>
+            )}
           </ul>
-        </ul>
-        <p>Black Owned: {studio.is_black_owned ? "Yes" : "No"}</p>
-      </div> */}
+        </div>
+        <div className="edit-button">
+          <Link to={`/products/${id}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+        <div className="delete-button">
+          <button>Delete</button>
+        </div>
+      </div>
     </>
   );
 };
